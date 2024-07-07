b00ce70098ea0f2701370a6058cc6f1969bceede
karinbar <karin.bar@checkmarx.com>
2024-07-07 10:27:03 +0300

    addon

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app: Application = express();
+const app: Express = express();
 app.use(cors());
 const port = process.env.PORT || 8000;
 
@@ -12,7 +12,17 @@ app.get('/', (req: Request, res: Response) => {
     console.log(">>")
     res.send('Welcome to Express & TypeScript Server');
 });
+app.get('/user/:id', (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    console.log(`Id=${id}`)
 
+    if (id) {
+        res.send(200);
+        //res.status(200).send('ok')
+    } else {
+        res.status(400).send('User not found')
+    }
+})
 app.get('/hello', (req: Request, res: Response) => {
     console.log(">>hello")
     res.send('Welcome to Express & TypeScript Server hello');
